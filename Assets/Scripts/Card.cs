0519af35dd6917be5e0a2644830d41f442c6d5da
zsun <zhongtang.sun@wsu.edu>
2021-11-08 18:27:25 -0800

    Testing commit

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This is the basic class for every cards.
+/// </summary>
+[CreateAssetMenu(fileName = "New Card", menuName = "Card/Software")]
+public class Card : ScriptableObject
+{
+    public string cardName;
+    public string cardDecrisption;
+    public string cardType;
+    public string cardRairty;
+
+    public int cardCost;
+    public int cardSerial;
+    
+
+   
+    public Sprite cardSprite;
+}
---
+++ Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour
+{
+    public Card card;
+
+    public Text costText;
+    public Text serialText;
+    public Text rairtyText;
+    public Text typeText;
+    public Text descriptionText;
+    public Text nameText;
+
+    public Image cardImage;
+
+    private void Start()
+    {
+        costText.text = card.cardCost.ToString();
+        serialText.text = card.cardSerial.ToString();
+
+        rairtyText.text = card.cardRairty;
+        typeText.text = card.cardType;
+        descriptionText.text = card.cardDecrisption;
+        nameText.text = card.cardName;
+
+        cardImage.sprite = card.cardSprite;
+    }
+
+}
+
